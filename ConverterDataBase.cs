6922669c9bb4fb0986f0770ee0871ce63830a61e
Zapyataya13 <63292225+Zapyataya13@users.noreply.github.com>
2021-07-01 00:36:06 +0400

    Converter

---
+++ ConverterDataBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Convertor
+{
+    public class ConverterDataBase
+    {
+        public Dictionary<int, double> fromMetresTo = new Dictionary<int, double>(8);
+
+        public ConverterDataBase()
+        {
+            fromMetresTo.Add(0, 0.001d); //km
+            fromMetresTo.Add(1, 1); //m
+            fromMetresTo.Add(2, 100); //cm
+            fromMetresTo.Add(3, 1000); //mm
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+
+namespace Convertor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConverterDataBase DataConvert = new ConverterDataBase();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickСalculate(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double fromNum;
+                fromNum = Double.Parse(userdata.Text);
+
+                if (fromNum < 0)
+                {
+                    Resul.Text = ("Введено некорректное число");
+                }
+                else
+                {
+                    Resul.Text = $"{CountResult(fromNum, Box_1.SelectedIndex, Box_2.SelectedIndex)}";
+                }
+            }
+            catch
+            {
+                Resul.Text = ("Введено некорректное число");
+            }
+        }
+
+        private double CountResult(double num, int fromType, int toType)
+        {
+            double res = num / DataConvert.fromMetresTo[fromType] * DataConvert.fromMetresTo[toType];
+            return res;
+        }
+    }
+}
